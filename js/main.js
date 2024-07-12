a8070cdcd3ea0637d9011ba2043ba1f8837ca077
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-07-12 15:12:31 +0200

    [UPD] Milestone1: created post structure into script

--- js/main.js
+++ js/main.js
@@ -61,5 +61,35 @@ const postContainer = document.getElementById('container');
 
 // creo la struttura HTML per ogni post utilizzando forEach
 posts.forEach((post) => {
-
+    
+    // aggiungo il post al contenitore
+    postContainer.innerHTML +=
+    `<div class="post">
+        <div class="post-header">
+            <div class="post-meta__icon">
+                <img class="profile-pic" src="${post.author.image}" alt="${post.author.name}">
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">${post.author.name}</div>
+                <div class="post-meta__time">4 mesi fa</div>
+            </div>
+        </div>
+        <div class="post__text">${post.content}</div>
+        <div class="post__image">
+            <img src="${post.media}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
+                </div>
+            </div> 
+        </div>
+    </div>`
 }) 
